fd64c27c67807829511d36db2e1cb9f8cba7d884
Manuel Ulate <fr3zs09@gmail.com>
2023-10-03 15:00:08 -0600

    new: null undefined types

--- 02-types/null-undefined.ts
+++ 02-types/null-undefined.ts
@@ -1,6 +1,9 @@
 (() => {
     //* Null y undefined
     // Representan la ausencia de valor
+    // undefined: se puede ver como un valor no definido por el momento
+    // pero se espera que tome un valor pronto
+    // null: representa la ausencia de valor, y no se sabe si tendrá uno
     let nothing: undefined = undefined;
 
     console.log( nothing );
