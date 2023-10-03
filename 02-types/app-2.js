08a4021954ff37241e4c841f05a382d6fb0c83a3
Manuel Ulate <fr3zs09@gmail.com>
2023-10-02 20:49:03 -0600

    new: types - function anonym

--- 02-types/app-2.js
+++ 02-types/app-2.js
@@ -1,15 +1,17 @@
 "use strict";
-//* Number
-//Cuando se crea una constante TS crea un nuevo tipo
-//en este caso se tiene que a:10 (Ya que el valor)
-//de las constantes no cambian
-const a = 10;
-//Cuando se define un var o let siempre es importante ponerle
-//el tipo de dato que almacena la variable
-let b = 10;
-//* Funcion
-//Para las funciones de igual manera se debe tipar los 
-//parametros que este puede recibir
-const sayHello = (msg) => {
-    console.log(msg);
-};
+(() => {
+    //* Number
+    //Cuando se crea una constante TS crea un nuevo tipo
+    //en este caso se tiene que a:10 (Ya que el valor)
+    //de las constantes no cambian
+    const a = 10;
+    //Cuando se define un var o let siempre es importante ponerle
+    //el tipo de dato que almacena la variable
+    let b = 10;
+    //* Funcion
+    //Para las funciones de igual manera se debe tipar los 
+    //parametros que este puede recibir
+    const sayHello = (msg) => {
+        console.log(msg);
+    };
+})();
--- 02-types/app-2.ts
+++ 02-types/app-2.ts
@@ -1,17 +1,20 @@
-//* Number
-//Cuando se crea una constante TS crea un nuevo tipo
-//en este caso se tiene que a:10 (Ya que el valor)
-//de las constantes no cambian
-const a = 10;
-
-//Cuando se define un var o let siempre es importante ponerle
-//el tipo de dato que almacena la variable
-let b:number = 10;
-
-
-//* Funcion
-//Para las funciones de igual manera se debe tipar los 
-//parametros que este puede recibir
-const sayHello = ( msg:string ) => {
-    console.log( msg );
-}
+(() => {
+    //* Number
+    //Cuando se crea una constante TS crea un nuevo tipo
+    //en este caso se tiene que a:10 (Ya que el valor)
+    //de las constantes no cambian
+    const a = 10;
+    
+    //Cuando se define un var o let siempre es importante ponerle
+    //el tipo de dato que almacena la variable
+    let b:number = 10;
+    
+    
+    //* Funcion
+    //Para las funciones de igual manera se debe tipar los 
+    //parametros que este puede recibir
+    const sayHello = ( msg:string ) => {
+        console.log( msg );
+    }
+    
+})()
