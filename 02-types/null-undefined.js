d7ea9ee54fc9b687b22122e2e5705df7a2242a94
Manuel Ulate <fr3zs09@gmail.com>
2023-10-03 14:59:01 -0600

    new: null undefined types

--- 02-types/null-undefined.js
+++ 02-types/null-undefined.js
@@ -1,7 +1,7 @@
 "use strict";
 (() => {
     //* Null y undefined
-    // 
+    // Representan la ausencia de valor
     let nothing = undefined;
     console.log(nothing);
 })();
--- 02-types/null-undefined.ts
+++ 02-types/null-undefined.ts
@@ -1,6 +1,6 @@
 (() => {
     //* Null y undefined
-    // 
+    // Representan la ausencia de valor
     let nothing: undefined = undefined;
 
     console.log( nothing );
