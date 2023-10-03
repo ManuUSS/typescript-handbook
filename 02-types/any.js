0144c35a497e800fe775f1a3e60efdbbd669b3e8
Manuel Ulate <fr3zs09@gmail.com>
2023-10-03 14:05:38 -0600

    new: any types js

--- 02-types/any.js
+++ 02-types/any.js
@@ -2,4 +2,17 @@
 (() => {
     //* Any
     //! Tratar de usar el tipo any la menor cantidad de veces posibles
+    let hero = 123;
+    //Cuando se crea una variable y no se asigna un valor 
+    //por defecto toma el tipo any
+    let exists;
+    let power;
+    //El any da más flexibilidad, pero pierda gran cantidad
+    //de beneficios que posee el tipado
+    hero = 'Dr Manuel';
+    console.log(hero.charAt(0)); //No intellisense
+    hero = 15.46465;
+    console.log(hero.toFixed(2)); //No intellisense
+    console.log(exists);
+    console.log(power);
 })();
