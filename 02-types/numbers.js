7cdb3382bc96d0ea32c94fae9d036319fd7ddd40
Manuel Ulate <fr3zs09@gmail.com>
2023-10-02 21:05:15 -0600

    new: numbers types ts

--- 02-types/numbers.js
+++ 02-types/numbers.js
@@ -4,8 +4,16 @@
     //Cuando se crea una constante TS crea un nuevo tipo
     //en este caso se tiene que a:10 (Ya que el valor)
     //de las constantes no cambian
-    const a = 10;
+    const TEN = 10;
     //Cuando se define un var o let siempre es importante ponerle
     //el tipo de dato que almacena la variable
-    let b = 10;
+    let avengers = 10;
+    const villians = 20;
+    //Permite el cambio ya que el tipo es el mismo
+    avengers = 123;
+    console.log({ avengers });
+    //Permite el cambio ya que el Number retorna un numero
+    //el NaN (Not a Number) también es considera un number
+    avengers = Number('55A');
+    console.log({ avengers });
 })();
--- 02-types/numbers.ts
+++ 02-types/numbers.ts
@@ -3,11 +3,21 @@
     //Cuando se crea una constante TS crea un nuevo tipo
     //en este caso se tiene que a:10 (Ya que el valor)
     //de las constantes no cambian
-    const a = 10;
+    const TEN = 10;
     
     //Cuando se define un var o let siempre es importante ponerle
     //el tipo de dato que almacena la variable
-    let b:number = 10;
+    let avengers:number = 10;
+    const villians: number = 20;
+
+    //Permite el cambio ya que el tipo es el mismo
+    avengers = 123;
+    console.log({ avengers });
     
+    //Permite el cambio ya que el Number retorna un numero
+    //el NaN (Not a Number) también es considera un number
+    avengers = Number('55A');
+    console.log({ avengers });
+
 
 })()
