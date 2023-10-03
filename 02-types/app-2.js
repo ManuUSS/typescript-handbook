e973704241056b55f3b45d02c1917ea7a7f79950
Manuel Ulate <fr3zs09@gmail.com>
2023-10-02 20:40:56 -0600

    new: types - number

--- 02-types/app-2.js
+++ 02-types/app-2.js
@@ -1,1 +1,8 @@
 "use strict";
+//Cuando se crea una constante TS crea un nuevo tipo
+//en este caso se tiene que a:10 (Ya que el valor)
+//de las constantes no cambian
+const a = 10;
+//Cuando se define un var o let siempre es importante ponerle
+//el tipo de dato que almacena la variable
+let b = 10;
