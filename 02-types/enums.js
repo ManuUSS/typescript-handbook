cdb62328a7990f045438b2c43518e62726231488
Manuel Ulate <fr3zs09@gmail.com>
2023-10-03 14:45:34 -0600

    new: enum types

--- 02-types/enums.js
+++ 02-types/enums.js
@@ -1,4 +1,14 @@
 "use strict";
 (() => {
     //* Enums
+    // Se define con la palabra reservada enum
+    // también es bueno utilizar UpperCamelCase
+    let AudioLevel;
+    (function (AudioLevel) {
+        AudioLevel["min"] = "Min";
+        AudioLevel["medium"] = "Med";
+        AudioLevel["max"] = "Max";
+    })(AudioLevel || (AudioLevel = {}));
+    let currentAudio = AudioLevel.medium;
+    console.log({ currentAudio });
 })();
--- 02-types/enums.ts
+++ 02-types/enums.ts
@@ -1,6 +1,14 @@
 (() => {
 
     //* Enums
-    
+    // Se define con la palabra reservada enum
+    // también es bueno utilizar UpperCamelCase
+    enum AudioLevel {
+        min = 'Min',
+        medium = 'Med',
+        max = 'Max'
+    }
 
+    let currentAudio = AudioLevel.medium;
+    console.log({ currentAudio });
 })()
