afd5849c2e9fb57cd0db135bc1520edb564453b7
Manuel Ulate <fr3zs09@gmail.com>
2023-10-02 20:47:20 -0600

    new: types - function

--- 02-types/app-2.js
+++ 02-types/app-2.js
@@ -1,4 +1,5 @@
 "use strict";
+//* Number
 //Cuando se crea una constante TS crea un nuevo tipo
 //en este caso se tiene que a:10 (Ya que el valor)
 //de las constantes no cambian
@@ -6,3 +7,9 @@ const a = 10;
 //Cuando se define un var o let siempre es importante ponerle
 //el tipo de dato que almacena la variable
 let b = 10;
+//* Funcion
+//Para las funciones de igual manera se debe tipar los 
+//parametros que este puede recibir
+const sayHello = (msg) => {
+    console.log(msg);
+};
