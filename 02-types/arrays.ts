acc306550810f05d0b57b2a6f9a2b558f2b0b2d4
Manuel Ulate <fr3zs09@gmail.com>
2023-10-03 14:27:47 -0600

    new: array types

--- 02-types/arrays.ts
+++ 02-types/arrays.ts
@@ -1,3 +1,19 @@
 (() => {
     //* Arrays
+    // Para definir arreglos se usan los []
+    // Generalmente se suelen utilizar de un solo tipo específico
+    const number:number[] = [ 1,2,3,4,5,6,7,8,9,10 ];
+    // Sin embargo también puede aceptar varios valores
+    const arrayVars:( string | number | boolean )[] = [ 1,2,3,4,5,6,7,'8',9,'10' ];
+    
+    // Al decir el tipo de elementos que acepta el array
+    // cualquier tipo que no haga match con el mecionado
+    // saltará un erro
+    number.push( 11 );
+    arrayVars.push( '11' );
+    arrayVars.push( true );
+
+    console.log( number );
+    console.log( arrayVars );
+
 })()
