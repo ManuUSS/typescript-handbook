6e8e8751bd8e605178cb131bf8883569bf61cb30
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 12:05:20 -0600

    new: funcs ts

--- 03-funcs/funcs.js
+++ 03-funcs/funcs.js
@@ -1,3 +1,19 @@
 "use strict";
 (() => {
+    //* Funciones
+    const hero = 'Manuel';
+    // Una función definida de manera normal
+    // se recomienda siempre utilizar el valor de retorno
+    function returnName() {
+        return hero;
+    }
+    // Una función de flecha definida de manera normal
+    // se recomienda siempre utilizar el valor de retorno
+    const activateSignal = () => {
+        return 'Activada';
+    };
+    // Los tipos de returnName y activateSignal son diferentes
+    console.log(typeof activateSignal);
+    // Asignando un valor con el valor de retorno de una función
+    const heroName = returnName();
 })();
--- 03-funcs/funcs.ts
+++ 03-funcs/funcs.ts
@@ -1,3 +1,25 @@
 (() => {
+    //* Funciones
+    
+    const hero:string = 'Manuel';
+
+    // Una función definida de manera normal
+    // se recomienda siempre utilizar el valor de retorno
+    function returnName():string {
+        return hero;
+    }
+
+    // Una función de flecha definida de manera normal
+    // se recomienda siempre utilizar el valor de retorno
+    const activateSignal = ():string => {
+        return 'Activada';
+    }
+
+    // Los tipos de returnName y activateSignal son diferentes
+    console.log( typeof activateSignal );
+
+    // Asignando un valor con el valor de retorno de una función
+    const heroName:string = returnName();
+
 
 })()
