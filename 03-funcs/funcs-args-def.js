33b17fecdddcbe239816c5ba27771ed43efe7574
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 14:22:03 -0600

    new: funcs args def ts

---
+++ 03-funcs/funcs-args-def.js
@@ -0,0 +1,20 @@
+"use strict";
+(() => {
+    //* Argumentos de las funciones
+    // Se definen los tipos de argumentos que espera la funcion
+    // para definir un argumento que no es obligatorio se utiliza
+    // el caracter ? el cual indica que es opcional
+    // el caracter = indica que el argumente tiene un valor por defecto
+    // en caso que un valor no sea enviado
+    const fullName = (firstName, lastName, upper = false) => {
+        // El operador || indica si no se tiene valor en lastName
+        // entonces que utilize ''
+        if (upper)
+            return `${firstName} ${lastName || ''}`.toUpperCase();
+        return `${firstName} ${lastName || ''}`;
+    };
+    // Se deben respetar los tipos de datos que espera,
+    // caso contrario muestra un error
+    const name = fullName('Manuel');
+    console.log(name);
+})();
