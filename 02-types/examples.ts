9be7d0a48b43480c8523c28e76540e32ec6ce40c
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 11:52:34 -0600

    new: examples ts

--- 02-types/examples.ts
+++ 02-types/examples.ts
@@ -1,35 +1,41 @@
 (() => {
 
     // Tipos
-    const batman   = 'Bruce';
-    const superman = 'Clark';
+    const batman:string   = 'Bruce';
+    const superman:string = 'Clark';
   
-    const existe = false;
+    const existe:boolean = false;
   
     // Tuplas
-    const parejaHeroes = [batman,superman];
-    const villano = ['Lex Lutor',5,true];
+    const parejaHeroes: [string, string] = [batman,superman];
+    const villano: [string, number, boolean] = ['Lex Lutor',5,true];
   
     // Arreglos
-    const aliados = ['Mujer Maravilla','Acuaman','San', 'Flash'];
+    const aliados:string[] = ['Mujer Maravilla','Acuaman','San', 'Flash'];
   
     //Enumeraciones
-    const fuerzaFlash = 5;
-    const fuerzaSuperman = 100;
-    const fuerzaBatman = 1;
-    const fuerzaAcuaman = 0;
+    enum Power {
+        PowerI = 0,
+        PowerII = 1,
+        PowerIII = 5,
+        PowerIV = 100
+    }
+    const fuerzaFlash = Power.PowerIII;
+    const fuerzaSuperman = Power.PowerIV;
+    const fuerzaBatman = Power.PowerII;
+    const fuerzaAcuaman = Power.PowerI;
   
     // Retorno de funciones
-    function activar_batiseñal(){
+    function activar_batiseñal():string {
       return 'activada';
     }
   
-    function pedir_ayuda(){
+    function pedir_ayuda():void{
       console.log('Auxilio!!!');
     }
   
     // Aserciones de Tipo
-    const poder: any = '100';
+    const poder: string = '100';
     const largoDelPoder:number = poder.length;
     console.log( largoDelPoder );
   
