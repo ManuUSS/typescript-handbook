9abfb846fae8cdee90433767acb8048304faa6bc
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 14:33:48 -0600

    new: funcs types ts

---
+++ 03-funcs/funcs-type.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    //* Funciones
+    const addNumber = (numA, numB) => numA + numB;
+    const greet = (name) => `Hola ${name}`;
+    const saveTheWorld = () => `El mundo está a salvo`;
+})();
---
+++ 03-funcs/funcs-type.ts
@@ -0,0 +1,11 @@
+(() => {
+
+    //* Funciones
+
+    const addNumber = ( numA: number, numB: number): number => numA + numB;
+    const greet = ( name:string ): string => `Hola ${ name }`;
+    const saveTheWorld = (): string => `El mundo está a salvo`;
+
+
+
+})()
