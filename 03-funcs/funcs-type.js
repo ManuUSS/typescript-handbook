ba25a2c140fe00d16e232cb0ef56b500dac4c444
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 14:40:44 -0600

    new: funcs types ts

--- 03-funcs/funcs-type.js
+++ 03-funcs/funcs-type.js
@@ -4,9 +4,13 @@
     const addNumbers = (numA, numB) => numA + numB;
     const greet = (name) => `Hola ${name}`;
     const saveTheWorld = () => `El mundo está a salvo`;
+    // Se define que es de tipo función
+    // let myFn: Function;
+    // Se define que es de tipo función
+    // que espera dos argumentos de tipo number y retorna un number
     let myFn;
-    myFn = 10;
-    console.log(myFn);
+    // Esto da un error por el tipado myFn = 10;
+    // console.log( myFn );
     // Pasa por referencia la funcion
     // al no utilizar los () se pasa la referencia
     // además, TS logra infererir el tipo de dato
@@ -15,11 +19,11 @@
     // Pasa por referencia la funcion
     // al no utilizar los () se pasa la referencia
     // además, TS logra infererir el tipo de dato
-    myFn = greet;
-    console.log(myFn('Manuel'));
+    //! Esto da error por el tipado: myFn = greet;
+    //! Esto da error por el tipado: console.log( myFn( 'Manuel' ) );
     // Pasa por referencia la funcion
     // al no utilizar los () se pasa la referencia
     // además, TS logra infererir el tipo de dato
-    myFn = saveTheWorld;
-    console.log(myFn());
+    //! Esto da error por el tipado: myFn = saveTheWorld;
+    //! Esto da error por el tipado: console.log( myFn() );
 })();
--- 03-funcs/funcs-type.ts
+++ 03-funcs/funcs-type.ts
@@ -6,11 +6,15 @@
     const greet = ( name:string ): string => `Hola ${ name }`;
     const saveTheWorld = (): string => `El mundo está a salvo`;
 
+    // Se define que es de tipo función
+    // let myFn: Function;
 
-    let myFn;
+    // Se define que es de tipo función
+    // que espera dos argumentos de tipo number y retorna un number
+    let myFn: ( a: number, b: number ) => number;
 
-    myFn = 10;
-    console.log( myFn );
+    // Esto da un error por el tipado myFn = 10;
+    // console.log( myFn );
     
     // Pasa por referencia la funcion
     // al no utilizar los () se pasa la referencia
@@ -21,14 +25,14 @@
     // Pasa por referencia la funcion
     // al no utilizar los () se pasa la referencia
     // además, TS logra infererir el tipo de dato
-    myFn = greet;
-    console.log( myFn( 'Manuel' ) );
+    //! Esto da error por el tipado: myFn = greet;
+    //! Esto da error por el tipado: console.log( myFn( 'Manuel' ) );
     
     // Pasa por referencia la funcion
     // al no utilizar los () se pasa la referencia
     // además, TS logra infererir el tipo de dato
-    myFn = saveTheWorld;
-    console.log( myFn() );
+    //! Esto da error por el tipado: myFn = saveTheWorld;
+    //! Esto da error por el tipado: console.log( myFn() );
 
 
 
