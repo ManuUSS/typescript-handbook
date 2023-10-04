c6852e30352de05738bef7e7333f5437aef8b8df
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 14:37:24 -0600

    new: funcs types ts

--- 03-funcs/funcs-type.js
+++ 03-funcs/funcs-type.js
@@ -1,7 +1,25 @@
 "use strict";
 (() => {
     //* Funciones
-    const addNumber = (numA, numB) => numA + numB;
+    const addNumbers = (numA, numB) => numA + numB;
     const greet = (name) => `Hola ${name}`;
     const saveTheWorld = () => `El mundo está a salvo`;
+    let myFn;
+    myFn = 10;
+    console.log(myFn);
+    // Pasa por referencia la funcion
+    // al no utilizar los () se pasa la referencia
+    // además, TS logra infererir el tipo de dato
+    myFn = addNumbers;
+    console.log(myFn(1, 5));
+    // Pasa por referencia la funcion
+    // al no utilizar los () se pasa la referencia
+    // además, TS logra infererir el tipo de dato
+    myFn = greet;
+    console.log(myFn('Manuel'));
+    // Pasa por referencia la funcion
+    // al no utilizar los () se pasa la referencia
+    // además, TS logra infererir el tipo de dato
+    myFn = saveTheWorld;
+    console.log(myFn());
 })();
--- 03-funcs/funcs-type.ts
+++ 03-funcs/funcs-type.ts
@@ -2,9 +2,33 @@
 
     //* Funciones
 
-    const addNumber = ( numA: number, numB: number): number => numA + numB;
+    const addNumbers = ( numA: number, numB: number): number => numA + numB;
     const greet = ( name:string ): string => `Hola ${ name }`;
     const saveTheWorld = (): string => `El mundo está a salvo`;
+
+
+    let myFn;
+
+    myFn = 10;
+    console.log( myFn );
+    
+    // Pasa por referencia la funcion
+    // al no utilizar los () se pasa la referencia
+    // además, TS logra infererir el tipo de dato
+    myFn = addNumbers;
+    console.log( myFn( 1, 5 ) );
+    
+    // Pasa por referencia la funcion
+    // al no utilizar los () se pasa la referencia
+    // además, TS logra infererir el tipo de dato
+    myFn = greet;
+    console.log( myFn( 'Manuel' ) );
+    
+    // Pasa por referencia la funcion
+    // al no utilizar los () se pasa la referencia
+    // además, TS logra infererir el tipo de dato
+    myFn = saveTheWorld;
+    console.log( myFn() );
 
 
 
