3df3faeb2b545f03907727cb8f6112af9da136da
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 12:10:01 -0600

    new: funcs args ts

--- 03-funcs/funcs-args.js
+++ 03-funcs/funcs-args.js
@@ -1,3 +1,11 @@
 "use strict";
 (() => {
+    //* Argumentos de las funciones
+    // Se definen los tipos de argumentos que espera la funcion
+    const fullName = (firstName, lastName) => {
+        return `${firstName} ${lastName}`;
+    };
+    // Se deben respetar los tipos de datos que espera
+    const name = fullName('Manuel', 'Ulate');
+    console.log(name);
 })();
--- 03-funcs/funcs-args.ts
+++ 03-funcs/funcs-args.ts
@@ -1,3 +1,14 @@
 (() => {
+    //* Argumentos de las funciones
+
+    // Se definen los tipos de argumentos que espera la funcion
+    const fullName = ( firstName:string, lastName: string ):string => {
+        return `${ firstName } ${ lastName }`;
+    }
+
+    // Se deben respetar los tipos de datos que espera
+    const name = fullName('Manuel', 'Ulate');
+    console.log( name );
+
 
 })();
