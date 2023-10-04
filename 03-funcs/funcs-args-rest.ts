fe39aae50680e4f2824d4bddf2cc259880e2eabb
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 14:29:22 -0600

    new: funcs args rest ts

--- 03-funcs/funcs-args-rest.ts
+++ 03-funcs/funcs-args-rest.ts
@@ -1,24 +1,14 @@
 (() => {
     //* Argumentos de las funciones
 
-    // Se definen los tipos de argumentos que espera la funcion
-    // para definir un argumento que no es obligatorio se utiliza
-    // el caracter ? el cual indica que es opcional
-    // el caracter = indica que el argumente tiene un valor por defecto
-    // en caso que un valor no sea enviado
-    // al tener un valor por defecto se considerada opcional
-    const fullName = ( firstName: string, lastName?: string, upper:boolean = false ):string => {
-        // El operador || indica si no se tiene valor en lastName
-        // entonces que utilize ''
-        if( upper )
-            return  `${ firstName } ${ lastName || '' }`.toUpperCase();
-        
-        return `${ firstName } ${ lastName || '' }`;
+    // El argumento rest indica que se pueden enviar x cantidad
+    // de argumentos no obligatorios, se define con los ...
+    // esto es un arreglo de tipo de args
+    const fullName = ( firstName: string, ...rest:string[] ):string => {
+        return `${ firstName } ${ rest.join(' ') }`;
     }
 
-    // Se deben respetar los tipos de datos que espera,
-    // caso contrario muestra un error
-    const name = fullName('Manuel');
+    const name = fullName('Manuel', 'Ulate', 'Sancho', 'Siu');
     console.log( name );
 
 
