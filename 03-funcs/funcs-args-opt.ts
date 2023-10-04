97935211db684631da6c7e7bd41feeb0e02ecd8e
Manuel Ulate <fr3zs09@gmail.com>
2023-10-04 12:15:05 -0600

    new: funcs args optionals ts

---
+++ 03-funcs/funcs-args-opt.ts
@@ -0,0 +1,17 @@
+(() => {
+    //* Argumentos de las funciones
+
+    // Se definen los tipos de argumentos que espera la funcion
+    // para definir un argumento que no es obligatorio se utiliza
+    // el caracter ? el cual indica que es opcional
+    const fullName = ( firstName: string, lastName?: string ):string => {
+        return `${ firstName } ${ lastName }`;
+    }
+
+    // Se deben respetar los tipos de datos que espera,
+    // caso contrario muestra un error
+    const name = fullName('Manuel');
+    console.log( name );
+
+
+})();
