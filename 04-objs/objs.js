9c3f0435f5bbc49951e8cc31f92bf6cb777d5fb8
Manuel Ulate <fr3zs09@gmail.com>
2023-10-06 09:41:29 -0600

    new: objs ts

--- 04-objs/objs.js
+++ 04-objs/objs.js
@@ -4,17 +4,15 @@
     // Se crea un objeto de similar manera,
     // se recomienda definir su tipo para respetar el tipado
     // esto se conoce como definición en línea
+    //? let flash: { name: string, age: number, powers: string[], getName?():string } = {
     let flash = {
         name: 'Barry Allen',
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo']
     };
-    flash = {
+    let superMan = {
         name: 'Clark Kent',
         age: 60,
-        powers: ['Super fuerza'],
-        getName() {
-            return this.name;
-        }
+        powers: ['Super fuerza']
     };
 })();
