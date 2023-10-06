addf04445d8b22bd98ebfee5956c7dccbc506747
Manuel Ulate <fr3zs09@gmail.com>
2023-10-06 09:44:03 -0600

    new: objs - types ts

--- 04-objs/objs-types.js
+++ 04-objs/objs-types.js
@@ -1,4 +1,17 @@
 "use strict";
 (() => {
     //* Types en objetos
+    // Se crea un objeto de similar manera,
+    // se recomienda definir su tipo para respetar el tipado
+    // de esta manera se utiliza un tipo y el código es más limpio
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    };
+    let superMan = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super fuerza']
+    };
 })();
--- 04-objs/objs-types.ts
+++ 04-objs/objs-types.ts
@@ -3,7 +3,25 @@
     //* Types en objetos
 
     type Hero = {
-        
+        name: string;
+        age: number;
+        powers: string[];
+        getName?:() => string;
+    }
+
+    // Se crea un objeto de similar manera,
+    // se recomienda definir su tipo para respetar el tipado
+    // de esta manera se utiliza un tipo y el código es más limpio
+    let flash: Hero = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    }
+
+    let superMan: Hero = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: [ 'Super fuerza' ]
     }
 
 })()
