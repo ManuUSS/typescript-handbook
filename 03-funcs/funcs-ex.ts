b0b5b9a740cba944889b67a5884cd46b23091da9
Manuel Ulate <fr3zs09@gmail.com>
2023-10-06 09:12:49 -0600

    new: functions ex ts

--- 03-funcs/funcs-ex.ts
+++ 03-funcs/funcs-ex.ts
@@ -1,36 +1,37 @@
 (() => {
 
     // Funciones Básicas
-    function sumar( a:number, b:number ){
+    function sumar( a:number, b:number ):number {
         return a + b;
     }
 
-    const contar = ( heroes ) => {
-    return heroes.length;
+    const contar = ( heroes:string[] ):number => {
+        return heroes.length;
     }
-    const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+
+    const superHeroes:string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
     contar(superHeroes);
 
     //Parametros por defecto
-    const llamarBatman = ( llamar ) => {
-    if( llamar ){
-        console.log("Batiseñal activada");
-    }
+    const llamarBatman = ( llamar:boolean = false ):void => {
+        if( llamar ){
+            console.log("Batiseñal activada");
+        }
     }
 
     llamarBatman();
 
     // Rest?
-    const unirheroes = ( personas ) => {
-    return personas.join(", ");
+    const unirheroes = ( ...personas:string[] ):string => {
+        return personas.join(", ");
     }
 
 
     // Tipo funcion
-    const noHaceNada = ( numero, texto, booleano, arreglo )=> {}
+    const noHaceNada = ( numero:number, texto:string, booleano:boolean, arreglo:string[] )=> {}
 
     // Crear el tipo de funcion que acepte la funcion "noHaceNada"
-    let noHaceNadaTampoco;
+    let noHaceNadaTampoco: ( num: number, txt: string, bool: boolean, arr: string [] ) => void;
     noHaceNadaTampoco = noHaceNada
 
 
