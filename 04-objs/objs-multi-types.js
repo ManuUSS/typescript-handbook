df54545e29227b5764ecb93d97e76bc18b2890e1
Manuel Ulate <fr3zs09@gmail.com>
2023-10-06 09:56:49 -0600

    new: objs - multiple types ts

--- 04-objs/objs-multi-types.js
+++ 04-objs/objs-multi-types.js
@@ -2,4 +2,13 @@
 (() => {
     //* Multiples Types en objetos
     let myCustomVar = "Manuel";
+    console.log(typeof myCustomVar);
+    myCustomVar = 20;
+    console.log(typeof myCustomVar);
+    myCustomVar = {
+        name: 'Manuel Ulate',
+        age: 20,
+        powers: ['Coding']
+    };
+    console.log(typeof myCustomVar);
 })();
--- 04-objs/objs-multi-types.ts
+++ 04-objs/objs-multi-types.ts
@@ -8,5 +8,17 @@
         getName?:() => string;
     }
 
-    let myCustomVar: string | Hero | number = "Manuel";
+    let myCustomVar: ( string | Hero | number ) = "Manuel";
+    console.log( typeof myCustomVar );
+    
+    myCustomVar = 20;
+    console.log( typeof myCustomVar );
+    
+    myCustomVar = {
+        name: 'Manuel Ulate',
+        age: 20,
+        powers: ['Coding']
+    }
+    console.log( typeof myCustomVar );
+
 })()
