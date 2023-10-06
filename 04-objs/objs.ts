204ffb74d6b7a0bbea931e288181782e2eed9e9a
Manuel Ulate <fr3zs09@gmail.com>
2023-10-06 09:26:24 -0600

    new: objs ts

---
+++ 04-objs/objs.ts
@@ -0,0 +1,17 @@
+(() => {
+
+    //* Objetos
+
+    // Al crear un objeto de esta manera TS infiere el tipo de dato
+    let flash = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    }
+
+    // Da error al no reconocer la nueva propiedad que se desea agregar
+    flash = {
+        lastName: 'Robertson'
+    }
+
+})()
