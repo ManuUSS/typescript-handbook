13b7eefcd128641d3364b37ca3cc06b30ab678a4
Manuel Ulate <fr3zs09@gmail.com>
2023-10-06 09:51:42 -0600

    new: objs - types ts

--- 04-objs/objs-types.js
+++ 04-objs/objs-types.js
@@ -9,9 +9,15 @@
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo']
     };
+    // Se crea un objeto de similar manera,
+    // se recomienda definir su tipo para respetar el tipado
+    // de esta manera se utiliza un tipo y el código es más limpio
     let superMan = {
         name: 'Clark Kent',
         age: 60,
-        powers: ['Super fuerza']
+        powers: ['Super fuerza'],
+        getName() {
+            return this.name;
+        }
     };
 })();
--- 04-objs/objs-types.ts
+++ 04-objs/objs-types.ts
@@ -18,10 +18,16 @@
         powers: ['Super velocidad', 'Viajar en el tiempo']
     }
 
+    // Se crea un objeto de similar manera,
+    // se recomienda definir su tipo para respetar el tipado
+    // de esta manera se utiliza un tipo y el código es más limpio
     let superMan: Hero = {
         name: 'Clark Kent',
         age: 60,
-        powers: [ 'Super fuerza' ]
+        powers: [ 'Super fuerza' ],
+        getName() {
+            return this.name;
+        }
     }
 
 })()
