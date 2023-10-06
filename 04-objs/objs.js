54690c18d107b65faa60bdee6b8b379942327121
Manuel Ulate <fr3zs09@gmail.com>
2023-10-06 09:37:35 -0600

    new: objs ts

--- 04-objs/objs.js
+++ 04-objs/objs.js
@@ -3,20 +3,18 @@
     //* Objetos
     // Se crea un objeto de similar manera,
     // se recomienda definir su tipo para respetar el tipado
+    // esto se conoce como definición en línea
     let flash = {
         name: 'Barry Allen',
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo']
     };
-    // Da error al no reconocer la nueva propiedad que se desea agregar
     flash = {
         name: 'Clark Kent',
         age: 60,
-        powers: ['Super fuerza']
-        /*
-        getName(){
+        powers: ['Super fuerza'],
+        getName() {
             return this.name;
         }
-        */
     };
 })();
--- 04-objs/objs.ts
+++ 04-objs/objs.ts
@@ -4,22 +4,21 @@
 
     // Se crea un objeto de similar manera,
     // se recomienda definir su tipo para respetar el tipado
-    let flash: { name: string, age: number, powers: string[] } = {
+    // esto se conoce como definición en línea
+    let flash: { name: string, age: number, powers: string[], getName?():string } = {
         name: 'Barry Allen',
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo']
     }
 
-    // Da error al no reconocer la nueva propiedad que se desea agregar
     flash = {
         name: 'Clark Kent',
         age: 60,
-        powers: ['Super fuerza']
-        /* 
+        powers: ['Super fuerza'], 
         getName(){
             return this.name;
         } 
-        */
+        
     }
 
 })()
