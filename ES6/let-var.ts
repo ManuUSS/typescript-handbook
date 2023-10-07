13966b38f2289fb1486a66190d63ee624b3bdc22
Manuel Ulate <fr3zs09@gmail.com>
2023-10-07 14:11:41 -0600

    new: es6 let var

--- ES6/let-var.ts
+++ ES6/let-var.ts
@@ -1,5 +1,22 @@
 (() => {
 
-    
+    // Evitar el uso del var
+    /* 
+        console.log( a );
+        var a = 'Manuel';
+    */
+
+    // Se recomienda siempre trabajar con constantes
+    // al menos que uno sepa que la asignación va a cambiar entonces usar let
+    const nombre = 'Manuel';
+    type Hero = {
+        a: number;
+        b: number;
+    }
+    const hero: Hero = {
+        a: 1,
+        b: 2
+    }
+    hero.a = 3;
 
 })()
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 "use strict";
 (() => {
+    const nombre = 'Manuel';
+    const hero = {
+        a: 1,
+        b: 2
+    };
+    hero.a = 3;
 })();
 //# sourceMappingURL=main.js.map
