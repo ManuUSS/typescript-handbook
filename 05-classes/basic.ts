96079bffdbdde0d2419a79b19c524a9f41c47de2
Manuel Ulate <fr3zs09@gmail.com>
2023-10-08 14:02:04 -0600

    new: classes initial

--- 05-classes/basic.ts
+++ 05-classes/basic.ts
@@ -3,25 +3,38 @@
     //* Clases
 
     class Avenger {
+        // ? Forma de definir el constructor estandar
         // La propiedad solo es visible dentro de la clase
-        private name:string;
+        // private name:string;
         // La propiedad es visible fuera de la clase
-        public team:string;
-        public realName:string;
+        // public team:string;
+        // public realName:string;
         // Solo puede ser accedida mediante la instacia de la clase
         static avgAge: number = 35;
 
         // Se crea un constructor de igual manera
-        constructor ( name:string, team:string, realName:string ) {
-            this.name = name;
-            this.team = team;
-            this.realName = realName;
+        /*
+            constructor ( name:string, team:string, realName:string ) {
+                this.name = name;
+                this.team = team;
+                this.realName = realName;
+            }
+        */
+
+        //? Forma más conocida
+        constructor ( 
+            private name:string, 
+            public team:string, 
+            public realName:string,
+            avgAge: number = 35 
+        ) {
+            Avenger.avgAge = avgAge;
         }
 
     }
 
     const antMan: Avenger = new Avenger( 'Manu', 'UCR', 'Manuel' );
-
+    console.log( antMan );
     console.log( Avenger.avgAge );
 
 })()
--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 "use strict";
 (() => {
     class Avenger {
-        constructor(name, team, realName) {
+        constructor(name, team, realName, avgAge = 35) {
             this.name = name;
             this.team = team;
             this.realName = realName;
+            Avenger.avgAge = avgAge;
         }
     }
     Avenger.avgAge = 35;
     const antMan = new Avenger('Manu', 'UCR', 'Manuel');
+    console.log(antMan);
     console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
