88ffa235fa249432a30a4c3fb9a8708117e2b06e
Manuel Ulate <fr3zs09@gmail.com>
2023-10-08 13:51:51 -0600

    new: classes initial

--- 05-classes/basic.ts
+++ 05-classes/basic.ts
@@ -2,6 +2,25 @@
 
     //* Clases
 
-    
+    class Avenger {
+        // La propiedad solo es visible dentro de la clase
+        private name:string;
+        // La propiedad es visible fuera de la clase
+        public team:string;
+        public realName:string;
+        // Solo puede ser accedida mediante la instacia de la clase
+        static avgAge: number = 35;
+
+        constructor ( name:string, team:string, realName:string ) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
+
+    }
+
+    const antMan: Avenger = new Avenger( 'Manu', 'UCR', 'Manuel' );
+
+    console.log( Avenger.avgAge );
 
 })()
--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 "use strict";
 (() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
+        }
+    }
+    Avenger.avgAge = 35;
+    const antMan = new Avenger('Manu', 'UCR', 'Manuel');
+    console.log(Avenger.avgAge);
 })();
 //# sourceMappingURL=main.js.map
