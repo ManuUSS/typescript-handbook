da212b6eff9ab878ad5bc112b43ab3e45cf5459e
Manuel Ulate <fr3zs09@gmail.com>
2023-10-10 14:21:14 -0600

    new: namespaces

--- 07-namespaces/space-basic.ts
+++ 07-namespaces/space-basic.ts
@@ -1,14 +1,22 @@
 namespace Validations {
 
-    const validateText = ( text: string ):boolean => {
+    //* Los namespaces no suelen ser vistos de manera ordinaria
+    //* generalmente son más utilizados por dentro de los frameworks
+    //* como Vue, Angular, React + TS, es decir, en la construcción de estos.
+
+    export const validateText = ( text: string ):boolean => {
         return ( text.length > 3 ) ? true : false;
     }
 
 
-    const validateDate = ( myDate: Date ):boolean => {
+    export const validateDate = ( myDate: Date ):boolean => {
         return ( isNaN( myDate.valueOf() ) )
             ? false
             : true
     }
 
 }
+
+console.log( Validations.validateText("Manu") );
+const d = new Date();
+console.log( Validations.validateDate( d ) );
