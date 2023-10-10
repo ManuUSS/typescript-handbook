693bc72c385758be9e5cd2432aec25df4495342e
Manuel Ulate <fr3zs09@gmail.com>
2023-10-09 20:29:53 -0600

    new: class private constructor

---
+++ 05-classes/private-constructor.ts
@@ -0,0 +1,13 @@
+(() => {
+
+    //* Constructor privado
+
+    // Se puede utilizar para controlar la manera en la que
+    // las instancias son ejecutadas
+    // uso similar al patron Singleton
+
+    class Apocalypes {
+        
+    }
+
+})()
