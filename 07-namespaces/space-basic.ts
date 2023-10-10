b946e9d506996205ccc247279ac58e14d46b8369
Manuel Ulate <fr3zs09@gmail.com>
2023-10-10 14:13:54 -0600

    new: namespaces

--- 07-namespaces/space-basic.ts
+++ 07-namespaces/space-basic.ts
@@ -1,7 +1,14 @@
 namespace Validations {
 
     const validateText = ( text: string ):boolean => {
-        return ( text.length > 3 );
+        return ( text.length > 3 ) ? true : false;
+    }
+
+
+    const validateDate = ( myDate: Date ):boolean => {
+        return ( isNaN( myDate.valueOf() ) )
+            ? false
+            : true
     }
 
 }
