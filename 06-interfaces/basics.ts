10e82e1dcf708207857b57516b1c6ce37c480410
Manuel Ulate <fr3zs09@gmail.com>
2023-10-09 20:48:08 -0600

    new: interfaces basics

--- 06-interfaces/basics.ts
+++ 06-interfaces/basics.ts
@@ -1,3 +1,22 @@
 (() => {
+    //* Intro a interfaces
 
+    // Las interfaces funcionan casi igual que los types
+    // tiene diferencias en tipado, sin embargo la más importante
+    // es que un type no puede ser extendible, un interface sí
+    interface Hero {
+        name: string;
+        age: number;
+        powers: string[];
+        getName?:() => string;
+    }
+
+    let superMan: Hero = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: [ 'Super fuerza' ],
+        getName() {
+            return this.name;
+        }
+    }
 })()
--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 "use strict";
 (() => {
+    let superMan = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super fuerza'],
+        getName() {
+            return this.name;
+        }
+    };
 })();
 //# sourceMappingURL=main.js.map
