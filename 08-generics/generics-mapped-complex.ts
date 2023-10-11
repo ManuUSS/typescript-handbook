3df0b19792f0a213d611d86e2f05240101a6de39
Manuel Ulate <fr3zs09@gmail.com>
2023-10-11 15:48:42 -0600

    new: generic mapped complex

--- 08-generics/generics-mapped-complex.ts
+++ 08-generics/generics-mapped-complex.ts
@@ -4,10 +4,46 @@
 
 
     // En ciertos escenarios es posible que queramos hacer tipos, más complejos
-    // como por ejemplo, dado un tipo mapear sus llaves y valores posibles
-    type MyInput<T> = {
-        // En este ejemplo
-        [ prop in keyof T ]: T[ prop ]
+    // como por ejemplo, dado un tipo mapear sus llaves y valores posibles y transformarlos
+    //? Para situaciones como las mencionadas no es posible utilizar interfaces, por lo que debemos usar map
+    type MyMappedGenericPartial<T> = {
+        // [ prop in keyof T ] Creo una llave que apunta a T
+        // T[ prop ] obtengo el tipo de valor que se guarda en esa llave
+        [ prop in keyof T ]?: T[ prop ];
     }
+    
+    type MyMappedGenericNumber<T> = {
+        // [ prop in keyof T ] Creo una llave que apunta a T
+        // T[ prop ] obtengo el tipo de valor que se guarda en esa llave
+        [ prop in keyof T ]?: number;
+    }
+
+    type MyMappedGenericConcrete<T> = {
+        // [ prop in keyof T ] Creo una llave que apunta a T
+        // T[ prop ] obtengo el tipo de valor que se guarda en esa llave
+        [ prop in keyof T ]-?: number;
+    }
+
+    interface User {
+        name: string;
+        userName:string;
+        email:string;
+        age: number;
+    }
+    interface UserMaybe {
+        name: string;
+        userName?:string;
+        email?:string;
+        age?: number;
+    }
+
+    // Todas las propiedades se transforman en opcional
+    type PartialUser = MyMappedGenericPartial<User>; 
+    
+    // Todas las propiedades se transforman a valor numerico
+    type NumberUser  = MyMappedGenericNumber<User>; 
+    
+    // Todas las propiedades se transforman a obligatorias
+    type ConcreteUser = MyMappedGenericConcrete<UserMaybe>; 
 
 })()
