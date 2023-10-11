1754b1640dcff8e6fbb0d41ab2df8e2e572a1f42
Manuel Ulate <fr3zs09@gmail.com>
2023-10-11 15:56:57 -0600

    new: generic mapped complex

--- 08-generics/generics-mapped-complex.ts
+++ 08-generics/generics-mapped-complex.ts
@@ -13,15 +13,22 @@
     }
     
     type MyMappedGenericNumber<T> = {
-        // [ prop in keyof T ] Creo una llave que apunta a T
-        // T[ prop ] obtengo el tipo de valor que se guarda en esa llave
+        // [ prop in keyof T ] Creo una llave (opcional) que apunta a T 
+        // digo que el valor de la llave es un number
         [ prop in keyof T ]?: number;
     }
 
     type MyMappedGenericConcrete<T> = {
-        // [ prop in keyof T ] Creo una llave que apunta a T
+        // [ prop in keyof T ] Creo una llave (requerida) que apunta a T
         // T[ prop ] obtengo el tipo de valor que se guarda en esa llave
         [ prop in keyof T ]-?: number;
+    }
+
+    type MyMappedGenericMethods<T> = {
+        // [ prop in keyof T ] Creo una llave que apunta a T
+        // con la palabra as renombro la llave del objeto
+        // () => T[ prop ] una funcion que retorna el tipo de valor de la llave
+        [ prop in keyof T as `get${ Capitalize<string & prop> }` ]: () => T[ prop ];
     }
 
     interface User {
@@ -45,5 +52,19 @@
     
     // Todas las propiedades se transforman a obligatorias
     type ConcreteUser = MyMappedGenericConcrete<UserMaybe>; 
+    
+    // Todas las propiedades se transforman a metodos
+    type GetsUser     = MyMappedGenericMethods<User>;
+    const getUser: GetsUser = {
+        getName: () => 'Manuel',
+        getUserName: () => 'iManu',
+        getEmail: () => 'manuel.gmail.com',
+        getAge:() => 20,
+    }
+
+    console.log( getUser.getName() ); // Manuel
+    console.log( getUser.getUserName() ); // 20
+    console.log( getUser.getEmail() ); // manuel.gmail.com
+    console.log( getUser.getAge() ); // 20
 
 })()
