a6e35eb4709030d6d2dea7cab49a7211d880e784
Manuel Ulate <fr3zs09@gmail.com>
2023-10-13 11:19:33 -0600

    new: decorators

--- 09-decorators/decorators.ts
+++ 09-decorators/decorators.ts
@@ -3,3 +3,10 @@ const printClg = ( constructor: Function ) => {
     console.log( constructor );
 }
 
+const printClgConditional = ( print: boolean = false ):Function => {
+    if( print ) {
+        return printClg
+    }
+    return () => {}
+}
+
